53895d5d6c66613f7bd9d2ac8cfb38592aae4a30
SebMed21 <sebastian.medina301426@gmail.com>
2024-10-31 21:57:41 +0800

    Updated main, added a break for the loop, updated pseudocode

--- name_range_input_main.py
+++ name_range_input_main.py
@@ -7,9 +7,6 @@ range_5 = []
 
 # create while loops
 while True:
-
-    while True:
-        
         # in the while loop prompt the user to input numbers
         try:
             number = int(input("Please enter a number : (1-50)"))
@@ -17,30 +14,33 @@ while True:
             # append the correct numbers into the corresponding list
             if number >= 1 and number <= 10:
                 range_1.append(number)
+                print("yes")
                 
-            if number >= 11 and number <= 20:
+            elif number >= 11 and number <= 20:
                 range_2.append(number)
+                print("yes")
                 
-            if number >= 21 and number <= 30:
+            elif number >= 21 and number <= 30:
                 range_3.append(number)
+                print("yes")
 
-            if number >= 31 and number <= 40:
+            elif number >= 31 and number <= 40:
                 range_4.append(number)
+                print("yes")
 
-            if number >= 41 and number <= 50:
+            elif number >= 41 and number <= 50:
                 range_5.append(number)
-            
-            # if the user inputs an incorrect number display the amount of numbers
-            else: number <= 0 or number >= 51
-            print(f"1 to 10: {range_1}")
-            print(f"11 to 20: {range_2}")
-            print(f"21 to 30: {range_3}")
-            print(f"31 to 40: {range_4}")
-            print(f"41 to 50: {range_5}")
+                print("yes")
 
+            # if the user inputs an incorrect number break the loop
+            elif number <= 0 or number >= 51:
+                break   
 
         except:
             break
-    
 
-# end the loop
+            #print(f"1 to 10: {range_1}")
+            #print(f"11 to 20: {range_2}")
+            #print(f"21 to 30: {range_3}")
+            #print(f"31 to 40: {range_4}")
+            #print(f"41 to 50: {range_5}")
