baa0bdc62793f0604342e74de02d3753aaf2333f
SebMed21 <sebastian.medina301426@gmail.com>
2024-10-31 19:12:24 +0800

    created main, added pseudocode

---
+++ name_range_input_main.py
@@ -0,0 +1,6 @@
+# create five list arrays
+# create while loops
+    # in the while loop prompt the user to input numbers
+    # append the correct numbers into the corresponding list
+    # if the user inputs an incorrect number display the amount of numbers
+# end the loop
