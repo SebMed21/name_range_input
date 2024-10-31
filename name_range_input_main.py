14f496cd80129e19e58f459e80e9c49da5d9b893
SebMed21 <sebastian.medina301426@gmail.com>
2024-10-31 22:16:42 +0800

    Updated main, added a way to count the numbers in the list, added a display of the lists when a user enters an invalid input

--- name_range_input_main.py
+++ name_range_input_main.py
@@ -14,23 +14,18 @@ while True:
             # append the correct numbers into the corresponding list
             if number >= 1 and number <= 10:
                 range_1.append(number)
-                print("yes")
                 
             elif number >= 11 and number <= 20:
                 range_2.append(number)
-                print("yes")
                 
             elif number >= 21 and number <= 30:
                 range_3.append(number)
-                print("yes")
 
             elif number >= 31 and number <= 40:
                 range_4.append(number)
-                print("yes")
 
             elif number >= 41 and number <= 50:
                 range_5.append(number)
-                print("yes")
 
             # if the user inputs an incorrect number break the loop
             elif number <= 0 or number >= 51:
@@ -39,8 +34,15 @@ while True:
         except:
             break
 
-            #print(f"1 to 10: {range_1}")
-            #print(f"11 to 20: {range_2}")
-            #print(f"21 to 30: {range_3}")
-            #print(f"31 to 40: {range_4}")
-            #print(f"41 to 50: {range_5}")
+#counts the inputs
+count_1 = len(range_1)
+count_2 = len(range_2)
+count_3 = len(range_3)
+count_4 = len(range_4)
+count_5 = len(range_5)
+#prints the count of the input
+print(f"1 to 10: {count_1}")
+print(f"11 to 20: {count_2}")
+print(f"21 to 30: {count_3}")
+print(f"31 to 40: {count_4}")
+print(f"41 to 50: {count_5}")
