cee33dc2d798e3e129db56adc141c478f6c593e4
SebMed21 <sebastian.medina301426@gmail.com>
2024-10-31 19:18:40 +0800

    updated main, added list and while loop

--- name_range_input_main.py
+++ name_range_input_main.py
@@ -1,5 +1,17 @@
 # create five list arrays
+range_1 = []
+range_2 = []
+range_3 = []
+range_4 = []
+range_5 = []
+
 # create while loops
+while True:
+    while True:
+        try:
+            number = int(input("Please enter a number : (1-50)"))
+        except:
+            break
     # in the while loop prompt the user to input numbers
     # append the correct numbers into the corresponding list
     # if the user inputs an incorrect number display the amount of numbers
